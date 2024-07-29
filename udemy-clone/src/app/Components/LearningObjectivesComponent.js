4dadc9aec339374fac3dece08b992cdee75e456e
Adham Sherif Abd El-Hamid <75391774+adhamsherif76262@users.noreply.github.com>
2024-07-29 20:47:51 +0300

    Updated LearningObjectivesComponent.js

--- udemy-clone/src/app/Components/LearningObjectivesComponent.js
+++ udemy-clone/src/app/Components/LearningObjectivesComponent.js
@@ -4,7 +4,7 @@ import Styles from "../Courses/[CourseID]/CourseDetails.module.css";
 function LearningObjectivesComponent({ CourseDetails, C }) {
   return (
     <div className={`${Styles.LearningObjectivesDiv}`}>
-      <h2 className={`${Styles.LearningObjectivesH2}`}>What you'll learn</h2>
+      <h2 className={`${Styles.LearningObjectivesH2}`}>What you will learn</h2>
       <ul className={`${Styles.LearningObjectivesUL}`}>
         {CourseDetails[C].learning_objective.map((item, index) => {
           return (
